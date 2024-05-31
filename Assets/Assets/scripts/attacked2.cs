4069458dff85bf1d57163109beb99fb04364d49e
eunji <dmdf0518@gmail.com>
2024-05-31 15:00:08 +0900

    Add Assets folder from monster(2)

---
+++ Assets/Assets/scripts/attacked2.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class attacked2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Animator animator;
+    void Start()
+    {
+        animator=GetComponent<Animator>();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Q))
+        {
+            animator.SetTrigger("TakeAttack");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            animator.SetTrigger("TakeDamage");
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            animator.SetTrigger("Dead");
+        }
+
+    }
+}
